67658d22669bde0d051923a2339ee89822547e26
ICSea <irinaca2@outlook.com>
2015-01-17 16:39:26 -0800

    calculate the inverse of a function or get it from cache if its there

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Function to cache the inverse of a matrix instead of doing the rather expensive computation every time
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## makeCacheMatrix: create list object to cache matrix inverse(set and get for the matrix and its inverse)
+makeCacheMatrix <- function(A = matrix()) {
+        m <- NULL
+        set <- function(B) {
+                A <<- B
+                m <<- NULL
+        }
+        get <- function() A
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cacheSolve: returns the inverse of the matrix. If first time called, calculate using solve(), else get it from cache
+cacheSolve <- function(A, ...) {
+        ##get the value from cache, and check if it's null
+        m <- A$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        ## if not cached, calculate it
+        data <- A$get()
+        m <- solve(data, ...)
+        A$setsolve(m)
+        m
 }
+
